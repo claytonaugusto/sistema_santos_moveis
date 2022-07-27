a4cd6cd8b300164d38d9c167defdf32bc9cca8b3
clayton <relacionamento@ag29.com.br>
2022-07-27 19:31:52 -0300

    creat login frot page

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Santos Móveis</title>
+</head>
+
+<body>
+    <div class="content">
+        <div class="login">
+            <div class="login-left">
+                <div class="login-left-text">
+                    <h2>Seja bem vindo!</h2>
+                    <p>Ao sistema de gestão de<br> vendas <strong>Santos Móveis!</strong></p>
+                </div>
+            </div>
+            <div class="login-right">
+                <div class="login-right-text">
+                    <h2>Login</h2>
+                    <form action="#" enctype="multipart/form-data">
+                        <input type="text" name="email" id="email">
+                        <input type="password" name="password" id="password">
+                        <input type="submit" value="Fazer login" id="login">
+                    </form>
+                    <a href="#">Esqueci minha senha</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
